f60112477e0676ad1f9806371ae1b0ba91055cac
Eduardo Barruzi <dutche@gmail.com>
2016-08-29 15:42:00 -0300

    rename_project should not alter Git files

--- rename_project.sh
+++ rename_project.sh
@@ -11,5 +11,5 @@ echo "Moving the project dir $OLD_NAME to $NEW_NAME"
 mv $OLD_NAME $NEW_NAME
 
 echo "Changing every entry of $OLD_NAME inside the project"
-grep -rl "$OLD_NAME" ./ | xargs sed -i "s/$OLD_NAME/$NEW_NAME/g"
+grep --exclude-dir=".*" -rl "$OLD_NAME" ./ | xargs sed -i "s/$OLD_NAME/$NEW_NAME/g"
 echo ""
